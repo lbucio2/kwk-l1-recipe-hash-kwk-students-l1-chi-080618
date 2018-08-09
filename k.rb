cb04e927cab17ac5ca5da86218b263b974c24851
Litzy Bucio <lbucio2@cps.edu>
2018-08-09 20:19:39 +0000

    automatically backed up by learn

---
+++ k.rb
@@ -0,0 +1,20 @@
+def user_ml
+  puts "Give me a verb"
+  verb1 = gets.chomp
+  puts "Give me a noun"
+  noun1 = gets.chomp
+  puts "Give me another noun"
+  noun2 = gets.chomp
+  puts "Give me an adjective"
+  adjective1 = gets.chomp
+  puts "Give me a place"
+  place1 = gets.chomp
+  puts "Give me a verb"
+  verb2 = gets.chomp
+  puts "Give me an adjective"
+  adjective2 = gets.chomp
+  
+  puts "Madison began #{verb1} becuase she was being chaesd by a #{noun1} and a #{noun2} she was so #{adjective1} and was confused as to why they were following her. Finally she found a #{place1} and hid there until she knew for a fact that they were gone. After that she was #{verb2} home to tell her family what crazy day she had. She bagan telling her parents the craxy story and they parents #{adjective2} to what Madison was tell them."
+end
+
+user_ml
---
+++ madlibs2.rb
@@ -0,0 +1,18 @@
+def user_ml
+  puts "Give me a verb"
+  verb1 = gets.chomp
+  puts "Give me a noun"
+  noun1 = get.chomp
+  puts "Give me another noun"
+  noun2 = gets.chomp
+  puts "Give me an adjective"
+  adjective1 = gets.chomp
+  puts "Give me a place"
+  place1 = gets.chomp
+  puts "Give me a verb"
+  verb2 = gets.chomp
+  
+  puts "Madison began #{verb1} becuase she was being chaesd by a #{noun1} and a #{noun2} she was so #{adjective1} and was confused as to why they were following her. Finally she found a #{place1} and hid there until she knew for a fact that they were gone. After that she was #{verb2} home to tell her family what crazy day she had."
+end
+
+user_ml
